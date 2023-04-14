ab4dce8cf05f3419b8d0c297f09d7bf1504a074a
Keaton <keatonrproud@gmail.com>
2023-04-14 13:01:29 +0200

    improving classification for positive/negative text

--- eval_requests.py
+++ eval_requests.py
@@ -2,7 +2,7 @@
 import flask.debughelpers
 from deep_translator import GoogleTranslator, exceptions
 from luga import language
-from flask import Flask, render_template, request, url_for, redirect, send_from_directory
+from flask import Flask, render_template, request, url_for, redirect
 import os
 import numpy as np
 import pandas as pd
@@ -13,6 +13,12 @@ app = Flask(__name__, template_folder='templates', static_folder='staticFiles')
 app.config['FILE_UPLOADS'] = 'flask_upload'
 
 sia = SentimentIntensityAnalyzer()
+urgent_words = ['need', 'urgent', 'urgency', 'urgently', 'please', 'help', 'useless', 'immediate', 'immediately',
+                'dire', 'asap', 'pay', 'paid', 'worst', 'worse', 'terrible', 'terribly', 'broke', 'broken',
+                'disappoint', 'disappointed', 'disappointingly', 'disappointing', 'quick', 'fast',
+                'dying', 'dead', 'death', 'kill', 'important', 'serious', 'pressing', 'now', 'fuck', 'hell', 'damn', 'shit', 'fucking']
+urgent_symbols = ['!', '$', '?']
+
 
 @app.route('/')
 def init():
@@ -35,7 +41,7 @@ def classify_string():
             msg = f"Too many requests for translations have been made through the API. Please try again in a few minutes"
             return render_template('classify_string_error.html', error=msg)
 
-        (sentiment, urgency, classification) = get_classification(trans_text)
+        (sentiment, urgency, classification) = get_classification_str(trans_text)
     else:
         msg = 'No string was entered.'
         return render_template('classify_string_error.html', error=msg)
@@ -46,6 +52,10 @@ def classify_string():
                                sentiment=sentiment,
                                urgency=urgency)
 
+@app.route('/mistake', methods=['POST','GET'])
+def mistake():
+    return render_template('index.html', title='hello')
+
 def translate_str(text):
     lang = language(text).name
     if lang == 'zh':
@@ -54,10 +64,11 @@ def translate_str(text):
 
 def v_polarity(text):
     return sia.polarity_scores(text)['compound']
+
 def v_negativity(text):
     return sia.polarity_scores(text)['neg']
 
-def get_classification(text):
+def get_classification_str(text):
 
     polarity = v_polarity(text)
     negativity = v_negativity(text)
@@ -85,14 +96,11 @@ def get_classification(text):
     elif high_urgency_max <= polarity and polarity <= mid_urgency_max:
         urgency = 3
     else:
-        urgency = -5
-
-    urgent_words = ['need', 'urgent', 'urgency', 'urgently', 'please', 'help', 'useless', 'immediate', 'immediately', 'dire', 'asap', 'pay', 'paid', 'worst', 'worse', 'terrible', 'terribly', 'broke', 'broken', 'disappoint', 'disappointed', 'disappointingly', 'disappointing', 'quick', 'fast', 'fuck', 'fucking', 'shit', 'damn', 'fucker', 'wrong', 'hell']
-    urgent_symbols = ['!', '$', '?']
+        urgency = 0
 
     count = 0
     for symbol in urgent_symbols:
-        count += text.count(symbol)/2
+        count += text.count(symbol)
 
     txt = ''.join([a for a in text if a.isalpha() or a == " "])
 
@@ -100,11 +108,16 @@ def get_classification(text):
     for word in urgent_words:
         count += to_check.count(word)
 
-    if polarity != 'Positive':
-        if count >= 3:
-            urgency += 2
-        elif count >= 1.5:
-            urgency += 1
+    if count >= 4:
+        urgency += 2
+    elif count >= 2:
+        urgency += 1
+
+    if sentiment == "Positive":
+        urgency *= 2
+
+    if sentiment == "Neutral":
+        urgency = (urgency-1)*2 + 1
 
     if urgency == 6:
         classification = "HIGH"
@@ -119,7 +132,7 @@ def get_classification(text):
     elif urgency == 1:
         classification = "Lowest"
     else:
-        classification = "No"
+        classification = "No Negativity or Urgency"
 
     return sentiment, urgency, classification
 
@@ -129,22 +142,31 @@ def classify_file():
         file = request.files['tickets_file']
     except flask.debughelpers.DebugFilesKeyError:
         print('error 1')
-        msg = "No file was uploaded."
+        msg = "No file was uploaded, or the file type was invalid."
         return render_template('classify_file_error.html', error=msg)
     path = os.path.join(app.config['FILE_UPLOADS'], file.filename)
     try:
         file.save(path)
     except FileNotFoundError:
         print('error 2')
-        msg = "No file was uploaded."
+        msg = "No file was uploaded, or the file type was invalid."
         return render_template('classify_file_error.html', error=msg)
     try:
         data = pd.read_csv(path, header=None)
     except UnicodeDecodeError:
         print('unicode error')
-        msg = "The file you have submitted is not encoded properly for the given text. Ensure it is saved as a .csv UTF-8 and not a normal .csv."
+        msg = "The file you have submitted is not encoded properly for the given text or does not meet the above criteria. Ensure it is saved as a .csv UTF-8 and not a normal .csv."
         return render_template('classify_file_error.html', error=msg)
 
+    classified_data = get_classification_file(data)
+
+    if request.method == 'POST':
+        return render_template('classify_file.html',
+                               row_data=list(classified_data.values.tolist()),
+                               column_names=classified_data.columns.values,
+                               zip=zip)
+
+def get_classification_file(data):
     data.rename(columns={0: "input"}, inplace=True)
 
     # translate data
@@ -152,34 +174,34 @@ def classify_file():
     data['input_english'] = data['input_english']
     data['polarity'] = data['input_english'].apply(v_polarity)
     data['negativity'] = data['input_english'].apply(v_negativity)
-    
+
     # get main sentiment ratings
     min_positive = 0.3
     min_neutral = 0
 
-    data['sentiment'] = np.select([((data['polarity'] > min_positive) & ((data['negativity'] == 0) | (data['polarity'] > 0.7))),
-                                         ((data['polarity'] < min_neutral) | (data['negativity'] > 0)),
-                                         ((min_positive >= data['polarity']) & (data['polarity'] >= min_neutral))],
-                                        ['Positive', 'Negative', 'Neutral'])
+    data['sentiment'] = np.select(
+        [((data['polarity'] > min_positive) & ((data['negativity'] == 0) | (data['polarity'] > 0.7))),
+         ((data['polarity'] < min_neutral) | (data['negativity'] > 0)),
+         ((min_positive >= data['polarity']) & (data['polarity'] >= min_neutral))],
+        ['Positive', 'Negative', 'Neutral'])
 
     high_urgency_max = -0.5
     mid_urgency_max = -0.35
     low_urgency_min = -0.2
 
     data['urgency_polarity'] = np.select([(data['polarity'] < high_urgency_max),
-                                           ((data['polarity'] > low_urgency_min) & (data['sentiment'] != "Positive")),
-                                           ((mid_urgency_max <= data['polarity']) & (data['polarity'] <= low_urgency_min)),
-                                           ((high_urgency_max <= data['polarity']) & (data['polarity'] <= mid_urgency_max))],
+                                          ((data['polarity'] > low_urgency_min) & (data['sentiment'] != "Positive")),
+                                          ((mid_urgency_max <= data['polarity']) & (
+                                                      data['polarity'] <= low_urgency_min)),
+                                          ((high_urgency_max <= data['polarity']) & (
+                                                      data['polarity'] <= mid_urgency_max))],
                                          [4, 1, 2, 3],
-                                         -5).astype(int)
-
-    urgent_words = ['need', 'urgent', 'urgency', 'urgently', 'please', 'help', 'useless', 'immediate', 'immediately', 'dire', 'asap', 'pay', 'paid', 'worst', 'worse', 'terrible', 'terribly', 'broke', 'broken', 'disappoint', 'disappointed', 'disappointingly', 'disappointing', 'quick', 'fast']
-    urgent_symbols = ['!', '$', '?']
+                                         0).astype(int)
 
     def urgent_word_count(text):
         count = 0
         for symbol in urgent_symbols:
-            count += text.count(symbol)/2
+            count += text.count(symbol)
 
         text = ''.join([a for a in text if a.isalpha() or a == " "])
 
@@ -189,56 +211,60 @@ def classify_file():
 
         return count
 
-
     # get counts of urgent text
     data['urgency_text'] = data['input_english'].apply(urgent_word_count)
 
     # # increase urgency level based on num of urgency text score
     if len(data) > 20:
-        urgency_text_level2 = data.urgency_text.iloc[round(len(data.urgency_text)*.1)]
-        urgency_text_level1 = data.urgency_text.iloc[round(len(data.urgency_text)*.25)]    
+        urgency_text_level2 = data.urgency_text.iloc[round(len(data.urgency_text) * .1)]
+        urgency_text_level1 = data.urgency_text.iloc[round(len(data.urgency_text) * .25)]
     else:
-        urgency_text_level2 = 3
-        urgency_text_level1 = 1
+        urgency_text_level2 = 4
+        urgency_text_level1 = 2
 
-    data['urgency_rating'] = np.where(((data['polarity'] != 'Positive') & (data['urgency_text'] > urgency_text_level2) & (data['urgency_polarity'] > 0)),
+    data['rating'] = np.where(((data['urgency_text'] > urgency_text_level2) & (data['urgency_polarity'] > 0)),
                                       data['urgency_polarity'] + 1,
                                       data['urgency_polarity'])
 
-    data['urgency_rating'] = np.where(((data['polarity'] != 'Positive') & (data['urgency_text'] > urgency_text_level1) & (data['urgency_polarity'] > 0)),
-                                      data['urgency_rating'] + 1,
-                                      data['urgency_rating'])
+    data['rating'] = np.where(((data['urgency_text'] > urgency_text_level1) & (data['urgency_polarity'] > 0)),
+                                      data['rating'] + 1,
+                                      data['rating'])
+
+    data['rating'] = np.where((data['polarity'] == 'Positive'),
+                                      data['rating'] * 2,
+                                      data['rating'])
+
+    data['rating'] = np.where((data['polarity'] == 'Neutral'),
+                                      (data['rating']-1)*2 + 1,
+                                      data['rating'])
 
     # create classification based on urgency ratings
-    data['classification'] = np.select([(data['urgency_rating'] == 6),
-                                       (data['urgency_rating'] == 5),
-                                       (data['urgency_rating'] == 4),
-                                       (data['urgency_rating'] == 3),
-                                       (data['urgency_rating'] == 2),
-                                       ((data['urgency_rating'] == 1) & (data['urgency_text'] > 0)),
-                                       ((data['urgency_rating'] == 1) & (data['urgency_text'] == 0))],
-                                      ['HIGH', 'HIGH', 'Mid-High', 'Low-Mid', 'Low', 'Lowest', 'Not Urgent'],
-                                      'Not Urgent')
+    data['classification'] = np.select([(data['rating'] == 6),
+                                        (data['rating'] == 5),
+                                        (data['rating'] == 4),
+                                        (data['rating'] == 3),
+                                        (data['rating'] == 2),
+                                        ((data['rating'] == 1) & (data['urgency_text'] > 0)),
+                                        ((data['rating'] == 1) & (data['urgency_text'] == 0))],
+                                       ['HIGH', 'HIGH', 'Mid-High', 'Low-Mid', 'Low', 'Lowest', 'No Negativity or Urgency'],
+                                       'No Negativity or Urgency')
 
     # make urgency classes sortable
     data.classification = pd.Categorical(data.classification,
-                                        categories=['Not Urgent', 'Lowest', 'Low', 'Low-Mid', 'Mid-High', 'HIGH'],
-                                        ordered=True)
+                                         categories=['No Negativity or Urgency', 'Lowest', 'Low', 'Low-Mid', 'Mid-High', 'HIGH'],
+                                         ordered=True)
     data.sentiment = pd.Categorical(data.sentiment,
                                     categories=['Negative', 'Neutral', 'Positive'],
                                     ordered=True)
 
-    data.sort_values(by='urgency_rating', ascending=False, inplace=True)
+    data.sort_values(by='rating', ascending=False, inplace=True)
 
-    data = data[['input', 'input_english', 'classification', 'sentiment', 'negativity', 'urgency_rating']]
+    data = data[['input', 'input_english', 'classification', 'sentiment', 'negativity', 'rating']]
 
     data.columns = data.columns.str.capitalize()
 
-    if request.method == 'POST':
-        return render_template('classify_file.html',
-                               row_data=list(data.values.tolist()),
-                               column_names=data.columns.values,
-                               zip=zip)
+    return data
+
 
 if __name__ == '__main__':
     app.run(debug=True)
